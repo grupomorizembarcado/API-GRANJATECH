e61a0e48cb3955e3abd19ee7c613b240b976279f
AlcilenySilva <capic.2023118tads0045@aluno.ifpi.edu.br>
2026-02-12 15:28:21 -0300

    feat: adicionando config de cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors"; 
 import siloRoutes from "./routes/silo.routes.js";
 import environmentRoutes from "./routes/environment.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
+
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",          
+     
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
+
 
 app.use("/api", siloRoutes);
 app.use("/api", environmentRoutes);
