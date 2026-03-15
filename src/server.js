88ff2e429b965bcb33b0669634d5d7822b94ac7c
RafhaelGaspar <102383572+Rafhael0069@users.noreply.github.com>
2026-03-15 16:31:09 -0300

    Adicionando permissao para a pagina online no CORS.

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",          
-     
+    "https://grupo-moriz-front.netlify.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
